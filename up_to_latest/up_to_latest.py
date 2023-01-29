7b8ee4d5c5010618fa3b9e4bf586f6116e6789fa
jeangjenq <jeangjenq@worf>
2023-01-29 18:52:41 +1300

    added dropdown and checkbox

--- up_to_latest/up_to_latest.py
+++ up_to_latest/up_to_latest.py
@@ -1,11 +1,9 @@
 import os
+import re
+import nuke
+from glob import glob
+from nukescripts.version import version_get, version_set
 from PySide2 import (QtUiTools, QtWidgets, QtCore)
-try:
-    import nuke
-    standalone = False
-except ModuleNotFoundError:
-    import sys
-    standalone = True
 
 class UpNodesToLatest(QtWidgets.QMainWindow):
     def __init__(self):
@@ -16,8 +14,8 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
 
     def load_ui(self):
         loader = QtUiTools.QUiLoader()
-        ui_path = os.path.join(os.path.dirname(__file__), "form.ui")
-        # ui_path = "/home/jeangjenq/repository/read_tools/up_to_latest/form.ui"
+        # ui_path = os.path.join(os.path.dirname(__file__), "form.ui")
+        ui_path = "/home/jeangjenq/repository/read_tools/up_to_latest/form.ui"
         ui_file = QtCore.QFile(ui_path)
         ui_file.open(QtCore.QFile.ReadOnly)
         self.ui = loader.load(ui_file)
@@ -28,8 +26,9 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
         self.setCentralWidget(self.ui)
         self.setWindowTitle("Upversion Nodes")
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-        self.setFixedHeight(640)
-        self.setFixedWidth(480)
+        # self.setFixedHeight(640)
+        # self.setFixedWidth(480)
+        self.setMinimumSize(self.table.sizeHint())
 
     def connect_ui(self):
         self.ui.buttonBox.accepted.connect(self.clickedOk)
@@ -37,28 +36,73 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
 
     def populate_table(self):
         # gather nodes
-        nodes = ["A", "B", "C"]
+        nodes = nuke.allNodes("Read")
         for node in nodes:
             # create table items
-            node_filename = QtWidgets.QTableWidgetItem(node)
-            c_version = QtWidgets.QTableWidgetItem("v001")
+            # get node basename only for ease of read
+            node_file = nuke.filename(node)
+            node_filename = os.path.basename(node_file)
+            filename_widget = QtWidgets.QTableWidgetItem(node_filename)
+            # get current version in nodegraph
+            (prefix, v) = version_get(node_filename, 'v')
+            c_version = "{}{}".format(prefix, v)
+            c_version_widget = QtWidgets.QTableWidgetItem(c_version)
             # gather all available versions
-            versions = ["v001", "v002", "v014"]
+            versions = self.detect_all_versions(node)
             versions_box = QtWidgets.QComboBox()
-            versions_box.addItems(versions)
-            versions_box.setCurrentText(versions[-1])
+            if versions:
+                versions_box.addItems(versions)
+                versions_box.setCurrentText(versions[-1])
+            else:
+                versions_box.addItem(c_version)
             # create checkbox
             check = QtWidgets.QTableWidgetItem()
             check.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
             check.setCheckState(QtCore.Qt.Checked)
-
+            # add all to table
+            # versions_box is a widget so use setCellWidget
             num_rows = self.table.rowCount()
             self.table.insertRow        (num_rows)
-            self.table.setItem          (num_rows, 0, node_filename)
-            self.table.setItem          (num_rows, 1, c_version)
+            self.table.setItem          (num_rows, 0, filename_widget)
+            self.table.setItem          (num_rows, 1, c_version_widget)
             self.table.setCellWidget    (num_rows, 2, versions_box)
             self.table.setItem          (num_rows, 3, check)
+        # auto stretch and resize
+        # working together seem to break one or another at the moment
+        self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.table.resizeColumnsToContents()
 
+    def detect_all_versions(self, node):
+        # detect all versions that exists in node folder
+        # nukescripts.version_latest() contains similar function
+        # but it's only friendly with v+1
+        # so if there's a version skip it stops at a certain version
+        # not my prefer method
+        versions = []
+        file = nuke.filename(node)
+        # regex patterns to replace versions and frame paddings to *
+        # we will use glob later to search for all files
+        versions_pattern = [r"v-?\d+"]
+        frame_paddings = [r"%\d+[dD]", r"\#+"]
+
+        for pattern in versions_pattern + frame_paddings:
+            file = re.sub(pattern, "*", file)
+        
+        # make sure all v### found in files are the same number
+        # don't know under what occasion you might have 2 different versions
+        # in same file path, don't know what to do about it
+        def all_same(items):
+            return all(x == items[0] for x in items)
+        # glob returns list of files that matches the pattern
+        # in this case probably all the individual frames
+        files = glob(file)
+        versions = []
+        for file in files:
+            find_version = re.findall(versions_pattern[0], file)
+            if find_version and all_same(find_version):
+                if find_version[0] not in versions:
+                    versions.append(find_version[0])
+        return versions
 
     def up_selected_nodes(self):
         up_nodes = []
@@ -77,13 +121,8 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
         self.close()
     
 
-if not standalone:
-    def UpNodesVersions():
-        UpNodesVersions.UpVersionsPanel = UpNodesToLatest()
-        UpNodesVersions.UpVersionsPanel.show()
+def UpNodesVersions():
+    UpNodesVersions.UpVersionsPanel = UpNodesToLatest()
+    UpNodesVersions.UpVersionsPanel.show()
+
     UpNodesVersions()
-else:
-    app = QtWidgets.QApplication(sys.argv)
-    window = UpNodesToLatest()
-    window.show()
-    app.exec_()
