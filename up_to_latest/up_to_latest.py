38e887cb1df2038ab53db3e734bd05bb78c55530
jeangjenq <jeangjenq@worf>
2023-02-05 05:02:50 +1300

    working action buttons and better initial resize

--- up_to_latest/up_to_latest.py
+++ up_to_latest/up_to_latest.py
@@ -1,9 +1,26 @@
 import os
 import re
 import nuke
+import platform
+import subprocess
 from glob import glob
 from nukescripts.version import version_get, version_set
 from PySide2 import QtUiTools, QtWidgets, QtCore, QtGui
+
+def open_folder(path):
+    '''
+    open folder from arg
+    '''
+    if not os.path.exists(os.path.abspath(path)):
+        path = os.path.dirname(path)
+    if platform.system() == "Windows":
+        os.startfile(os.path.abspath(path))
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    elif platform.system() == "Linux":
+        subprocess.check_call(["xdg-open", path])
+    else:
+        nuke.message("Unsupported OS")
 
 class UpNodesToLatest(QtWidgets.QMainWindow):
     def __init__(self):
@@ -11,6 +28,8 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
         self.load_ui()
         self.populate_table()
         self.connect_ui()
+        # self.setMinimumSize(self.table.sizeHint())
+        self.resize_table_to_contents()
 
     def load_ui(self):
         loader = QtUiTools.QUiLoader()
@@ -24,21 +43,20 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
         
         # set window to use loaded ui file as central widget
         self.setCentralWidget(self.ui)
-        self.setWindowTitle("Upversion Nodes")
+        self.setWindowTitle("Versions")
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-        # self.setFixedHeight(640)
-        # self.setFixedWidth(480)
-        self.setMinimumSize(self.table.sizeHint())
 
     def connect_ui(self):
         self.ui.buttonBox.accepted.connect(self.clickedOk)
         self.ui.buttonBox.rejected.connect(self.clickedCancel)
 
     def populate_table(self):
-        # populate table with all nodes and its latest version
-        # include checkbox to determine whether to skip some nodes
-        # color change on out of version nodes
-        # gather nodes
+        '''
+        populate table with all nodes and its latest version
+        include checkbox to determine whether to skip some nodes
+        color change on out of version nodes
+        gather nodes
+        '''
         nodes = nuke.allNodes("Read")
         for node in nodes:
             # create table items
@@ -66,20 +84,25 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
                 versions_box.addItem(c_version)
 
             # create update checkbox
-            check_widget = QtWidgets.QTableWidgetItem("Update this node")
+            check_widget = QtWidgets.QTableWidgetItem()
             check_widget.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
             check_widget.setCheckState(QtCore.Qt.Checked)
 
             # create some action buttons
             focus_button = QtWidgets.QPushButton("Focus")
             openf_button = QtWidgets.QPushButton("Open Folder")
-            focus_button.clicked.connect(self.focus_on_node)
-            openf_button.clicked.connect(self.open_node_folder)
+            # connect action buttons
+            focus_button.clicked.connect(lambda *args,
+                                                 node=node:
+                                                 self.focus_on_node(node))
+            openf_button.clicked.connect(lambda *args,
+                                                 path = os.path.dirname(node_file):
+                                                 open_folder(path))
             actions_layout = QtWidgets.QHBoxLayout()
             for widget in [focus_button, openf_button]:
                 widget.setSizePolicy(   QtWidgets.QSizePolicy.Expanding,
                                         QtWidgets.QSizePolicy.Expanding)
-                widget.setMinimumSize(20, 20)
+                widget.setMinimumSize(20, 16)
                 actions_layout.addWidget(widget)
             actions_widget = QtWidgets.QWidget()
             actions_widget.setLayout(actions_layout)
@@ -87,7 +110,7 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
             # change row color if version outdated
             if versions_box.currentText() != c_version:
                 for item in [filename_widget, c_version_widget, check_widget]:
-                    item.setForeground(QtCore.Qt.red)
+                    item.setForeground(QtCore.Qt.yellow)
 
             # add all to table
             # versions_box is a widget so use setCellWidget
@@ -96,29 +119,31 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
             filename =  self.table.setItem          (num_rows, 0, filename_widget)
             current =   self.table.setItem          (num_rows, 1, c_version_widget)
             latest =    self.table.setCellWidget    (num_rows, 2, versions_box)
-            tickbox =   self.table.setItem          (num_rows, 3, check_widget)
-            actions =   self.table.setCellWidget    (num_rows, 4, actions_widget)
-
+            actions =   self.table.setCellWidget    (num_rows, 3, actions_widget)
+            tickbox =   self.table.setItem          (num_rows, 4, check_widget)
 
         # auto stretch and resize
         # self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         self.table.resizeColumnsToContents()
 
     def detect_all_versions(self, node):
-        # detect all versions that exists in node folder
-        # nukescripts.version_latest() contains similar function
-        # but it's only friendly with v+1
-        # so if there's a version skip it stops at a certain version
-        # not my prefer method
+        '''
+        detect all versions that exists in node folder
+        nukescripts.version_latest() contains similar function
+        but it's only friendly with v+1
+        so if there's a version skip it stops at a certain version
+        not my prefer method
+        '''
         versions = []
         file = nuke.filename(node)
         # regex patterns to replace versions and frame paddings to *
         # we will use glob later to search for all files
-        versions_pattern = [r"v-?\d+"]
+        versions_pattern = r"v-?\d+"
         frame_paddings = [r"%\d+[dD]", r"\#+"]
 
-        for pattern in versions_pattern + frame_paddings:
-            file = re.sub(pattern, "*", file)
+        file = re.sub(versions_pattern, "v*", file)
+        for pattern in frame_paddings:
+            file = re.sub(pattern, "[0-9]*", file)
         
         # make sure all v### found in files are the same number
         # don't know under what occasion you might have 2 different versions
@@ -130,14 +155,14 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
         files = glob(file)
         versions = []
         for file in files:
-            find_version = re.findall(versions_pattern[0], file)
+            find_version = re.findall(versions_pattern, file)
             if find_version and all_same(find_version):
                 if find_version[0] not in versions:
                     versions.append(find_version[0])
         return versions
     
     def up_selected_nodes(self):
-        # set version if upversion checkbox is check
+        # set version if uresize_table_to_contentspversion checkbox is check
         up_nodes = []
         for row in range(self.table.rowCount()):
             checkbox = self.table.item(row, self.table.columnCount()-1)
@@ -160,14 +185,24 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
             new_file = version_set(orig_file, "v", int(current), int(set_to))
             node['file'].setValue(new_file)
 
-    def focus_on_node(self):
-        sender = self.sender()
-        nuke.zoomToFitSelected()
-        print(sender)
+    def focus_on_node(self, node):
+        '''
+        focus on node, expect node in argument
+        '''
+        nuke.zoom(1, [node.xpos(), node.ypos()])
+        # nuke.zoomToFitSelected()
 
     def open_node_folder(self):
         sender = self.sender()
         print(sender)
+
+    def resize_table_to_contents(self):
+        vh = self.table.verticalHeader()
+        hh = self.table.horizontalHeader()
+        size = QtCore.QSize(hh.length(), vh.length())  # Get the length of the headers along each axis.
+        size += QtCore.QSize(vh.size().width(), hh.size().height())  # Add on the lengths from the *other* header
+        size += QtCore.QSize(100, 100)  # Extend further so scrollbars aren't shown.
+        self.resize(size)
 
     def clickedOk(self):
         self.up_selected_nodes()
@@ -180,5 +215,9 @@ class UpNodesToLatest(QtWidgets.QMainWindow):
 def UpNodesVersions():
     UpNodesVersions.UpVersionsPanel = UpNodesToLatest()
     UpNodesVersions.UpVersionsPanel.show()
+
+def createPanel():
+    pane = nuke.getPaneFor("example.test.panel")
+    panels.registerWidgetAsPanel('UpNodesToLatest', 'UpNodesToLatest',"example.test.panel", True).addToPane(pane)
 
 UpNodesVersions()
