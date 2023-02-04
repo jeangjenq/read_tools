9787e141b3bb2f637e44574cf06cfb6cd3df0ee0
jeangjenq <jeangjenq@worf>
2023-02-05 05:03:14 +1300

    more accurate finding versions

--- up_to_latest/find_versions.py
+++ up_to_latest/find_versions.py
@@ -1,13 +1,15 @@
 import os
 import re
 from glob import glob
-path = '/home/jeangjenq/repository/read_up_version/media/checker_v001/checker_v001.%04d.exr'
+path = r'/home/jeangjenq/repository/read_up_version/media/checker_v001/checker_v001.%04d.exr'
 
-versions_pattern = [r"v-?\d+"]
+versions_pattern = r"(?P<version>[vV])(-?\d+)"
 frame_paddings = [r"%\d+[dD]", r"\#+"]
 
-for pattern in versions_pattern + frame_paddings:
-    path = re.sub(pattern, "*", path)
+path = re.sub(versions_pattern, "\g<version>*", path)
+
+for frame in frame_paddings:
+    path = re.sub(frame, "\g<version>*", path)
 
 def all_same(items):
     return all(x == items[0] for x in items)
@@ -16,7 +18,7 @@ files = glob(path)
 print(files)
 versions = []
 for file in files:
-    find_version = re.findall(versions_pattern[0], file)
+    find_version = re.findall(versions_pattern, file)
     if find_version and all_same(find_version):
         if find_version[0] not in versions:
             versions.append(find_version[0])
