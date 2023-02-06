c6fcccb57ddc04dd24f2d4d859a5f68e5098f2af
jeangjenq <jeangjenq@worf>
2023-02-06 22:13:39 +1300

    added nodes_set_version

--- menu.py
+++ menu.py
@@ -1,13 +1,15 @@
 import readTools
+import nodes_set_version
 
 #Create toolbar with icon
 toolbar = nuke.menu('Nodes').addMenu('jj_tools', icon='icon_JJ.png')
 
 #Python menu
-Pytmenu = toolbar.addMenu('Read Tools')
-Pytmenu.addCommand('Select Read Nodes', 'readTools.selectRead()', index=0)
-Pytmenu.addCommand('Set nodes localization', 'readTools.setLocalize()')
-Pytmenu.addCommand('Set read nodes frame range', 'readTools.setFrameRange()')
-Pytmenu.addCommand('Missing frames setting', 'readTools.setError()')
-Pytmenu.addCommand('Reload selected reads', 'readTools.refreshReads()')
-Pytmenu.addCommand('Delete Files', 'readTools.deleteFiles()')
+read_tools_menu = toolbar.addMenu('Read Tools')
+read_tools_menu.addCommand('Select Read Nodes', 'readTools.selectRead()', index=0)
+read_tools_menu.addCommand('Set nodes localization', 'readTools.setLocalize()')
+read_tools_menu.addCommand('Set read nodes frame range', 'readTools.setFrameRange()')
+read_tools_menu.addCommand('Missing frames setting', 'readTools.setError()')
+read_tools_menu.addCommand('Reload selected reads', 'readTools.refreshReads()')
+read_tools_menu.addCommand('Nodes version', 'nodes_set_version.NodesVersions()')
+read_tools_menu.addCommand('Delete Files', 'readTools.deleteFiles()')
