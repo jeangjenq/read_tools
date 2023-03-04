063ef4f57f1a9e1bea6daa6b145ef7b0490dd994
jeangjenq <jeangjenq@worf>
2023-03-05 00:30:50 +1300

    mv readTools to read_knobs as it change read knobs

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
-import readTools
+# load read knobs
+nuke.pluginAddPath("./read_knobs")
+import read_knobs
 
 # load versions table
 nuke.pluginAddPath("./versions_table")
@@ -9,10 +11,10 @@ toolbar = nuke.menu('Nodes').addMenu('jj_tools', icon='icon_JJ.png')
 
 #Python menu
 read_tools_menu = toolbar.addMenu('Read Tools')
-read_tools_menu.addCommand('Select Read Nodes', 'readTools.selectRead()', index=0)
-read_tools_menu.addCommand('Set nodes localization', 'readTools.setLocalize()')
-read_tools_menu.addCommand('Set read nodes frame range', 'readTools.setFrameRange()')
-read_tools_menu.addCommand('Missing frames setting', 'readTools.setError()')
-read_tools_menu.addCommand('Reload selected reads', 'readTools.refreshReads()')
+read_tools_menu.addCommand('Select Read Nodes', 'read_knobs.selectRead()', index=0)
+read_tools_menu.addCommand('Set nodes localization', 'read_knobs.setLocalize()')
+read_tools_menu.addCommand('Set read nodes frame range', 'read_knobs.setFrameRange()')
+read_tools_menu.addCommand('Missing frames setting', 'read_knobs.setError()')
+read_tools_menu.addCommand('Reload selected reads', 'read_knobs.refreshReads()')
 read_tools_menu.addCommand('Nodes version', 'versions_table.NodesVersions()')
-read_tools_menu.addCommand('Delete Files', 'readTools.deleteFiles()')
+read_tools_menu.addCommand('Delete Files', 'read_knobs.deleteFiles()')
